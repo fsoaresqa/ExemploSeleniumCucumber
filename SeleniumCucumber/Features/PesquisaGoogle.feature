8d778d34c787437bd4885c286c146d67fb07c7df
Fernando <nandophantom@gmail.com>
2020-07-31 15:35:15 -0300

    apenas um exemplo

---
+++ SeleniumCucumber/Features/PesquisaGoogle.feature
@@ -0,0 +1,16 @@
+#language: pt
+
+
+Funcionalidade: Realizar uma pesquisa no google
+
+
+  Como internauta eu quero realizar uma pesquisa no site Google
+  De modo que eu tenha o resultado esperado
+
+
+  Cenario: Pesquisa com sucesso
+
+    Dado que eu acesse Google.com
+    Quando realizar a pesquisa
+    Entao pesquisa e realiada com sucesso
+
---
+++ SeleniumCucumber/src/test/java/StepsDefinitions/PesquisaGoogleSteps.java
@@ -0,0 +1,40 @@
+    package StepsDefinitions;
+
+    import io.cucumber.java.es.Dado;
+    import io.cucumber.java.it.Quando;
+    import io.cucumber.java.pt.Entao;
+    import org.junit.Assert;
+    import org.openqa.selenium.By;
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.chrome.ChromeDriver;
+
+    public class PesquisaGoogleSteps {
+
+    WebDriver driver;
+
+        @Dado("que eu acesse Google.com")
+        public void que_eu_acesse_google_com() {
+            System.setProperty("driver.chrome.driver", "C:/chromederiver.exe");
+
+            driver =  new ChromeDriver();
+
+            driver.get("https://www.google.com");
+        }
+
+
+        @Quando("realizar a pesquisa")
+        public void realizar_a_pesquisa() {
+            driver.findElement(By.name("q")).sendKeys("Covi-19");
+            driver.findElement(By.id("lga")).click();
+            driver.findElement(By.cssSelector("#tsf > div:nth-child(2) > div.A8SBwf > div.FPdoLc.tfB0Bf > center > input.gNO89b")).click();
+        }
+
+        @Entao("pesquisa e realiada com sucesso")
+        public void pesquisa_e_realiada_com_sucesso() {
+        boolean resultado = driver.findElement(By.cssSelector("#Yf1RJc > div.K20DDe.B8DMnc.JXFbbc.IDHhnb > div > div > div > div.PyJv1b.gsmt.PZPZlf > span")).isDisplayed();
+            Assert.assertEquals(true, resultado);
+
+
+            }
+
+    }
